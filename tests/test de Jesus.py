b1c2d62cee225ba2b8299d81e9bad3715919efa4
Jesus Ibarra <ayudamedezmayo@gmail.com>
2026-08-24 11:57:00 -0600

    test
    test 1

---
+++ tests/test de Jesus.py
@@ -0,0 +1,8 @@
+def main():
+    nombre = input("Ingresa tu nombre: ")
+    edad = input("Ingresa tu edad: ")
+
+    print(f"Tu nombre es {nombre} y tu edad es {edad}")
+
+if __name__ == "__main__":
+    main()
