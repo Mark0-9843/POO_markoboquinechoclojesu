0ed1dd68c35a5cfe9d15df558cc1d2a6f68bae4a
Jesus Ibarra <ayudamedezmayo@gmail.com>
2026-08-24 12:12:36 -0600

    Update test de Jesus.py

--- tests/test de Jesus.py
+++ tests/test de Jesus.py
@@ -1,3 +1,8 @@
+"""test de Jesus.py
+   Codigo que permite ingresar datos y que se impriman en la consola
+   Autor: Jose de Jesus Ibarra Guzman/mookitagit"""
+
+
 def main():
     nombre = input("Ingresa tu nombre: ")
     edad = input("Ingresa tu edad: ")
